89a728e563a98a4229efa62af45966ae6e11c0ad
Carter <wjhsmarter@126.com>
2019-05-05 12:06:49 +0800

    1、完成用户vip页面 2、完成我的收藏页面 3、更新iconfont图标 4、新增uni-card组件

--- bid-system-ui/pages-user-center/collection/collection.vue
+++ bid-system-ui/pages-user-center/collection/collection.vue
@@ -1,21 +1,250 @@
 <template>
 	<view>
-		我的收藏
+		<!-- 分类导航栏 -->
+		<view class="uni-tab-bar zy-tab-bar">
+			<scroll-view id="tab-bar" class="uni-swiper-tab" scroll-x :scroll-left="projectType.scrollLeft">
+				<view v-for="(tab,index) in projectType.tabbars" :key="tab.id" class="swiper-tab-list" :class="projectType.tabIndex==index ? 'active' : ''"
+				 :id="tab.id" :data-current="index" @click="tapTab">{{tab.name}}</view>
+			</scroll-view>
+		</view>
+		
+		<!-- 类别标签页 -->
+		<view style="background-color: #FFFFFF; padding-bottom: 10upx;">
+			<uni-segmented-control :current="projectStatus.current" :values="projectStatus.items" v-on:clickItem="onClickItem" styleType="button" activeColor="#108EE9"></uni-segmented-control>
+		</view>
+		
+		<!-- 列表 -->
+		<view class="zy-page-list zy-project" v-if="projects.length > 0">
+			<view class="zy-page-list-item" v-for="(project, index) in projects" :key="index" >
+				<view @click="toProjectDetail">
+					<view class="zy-project-head">
+						<image class="zy-icon" :src="imgIcon"/>
+						<view>
+							<view>
+								<text>{{projectTypeName}}</text>
+								<text style="margin-left: 30upx;">[{{project.city}}]</text>
+							</view>
+							<view class="zy-text-mini zy-text-info">公告时间：{{project.publishTime}}</view>
+						</view>
+						<view class="zy-project-head-right">
+							<view>
+								<uni-tag text="最新" type="error" size="small" :inverted="true" :circle="true"></uni-tag>
+								<uni-tag :text="project.status" type="primary" size="small" :inverted="true" :circle="true" style="margin-left: 10upx;"></uni-tag>
+							</view>
+							<view class="zy-text-mini zy-text-warning" v-if="project.time !== null">开标时间：{{project.time}}</view>
+							<view class="zy-text-mini zy-text-warning" v-else>开标时间：暂无</view>
+						</view>
+					</view>
+					<view>
+						<view class="zy-text-big zy-text-bold">{{project.title}}</view>
+						<view class="zy-text-info"><text class="zy-text-info zy-text-bold">招标单位：</text>{{project.title}}</view>
+						<view class="zy-text-info"><text class="zy-text-info zy-text-bold">企业资质：</text>{{project.title}}</view>
+						<view class="zy-text-info"><text class="zy-text-info zy-text-bold">建造师等级：</text>{{project.title}}</view>
+						<view class="zy-project-item-row">
+							<view class="zy-text-info"><text class="zy-text-info zy-text-bold">审查方式：</text>{{project.checkWay}}</view>
+							<view class="zy-text-info"><text class="zy-text-info zy-text-bold">项目投资：</text>{{project.money}}</view>
+						</view>
+					</view>
+				</view>
+				<view class="zy-project-item-file">
+					<uni-tag text="澄" type="error" size="small" :inverted="true" :circle="false" @click="toFile(0)"></uni-tag>
+					<uni-tag text="招" type="error" size="small" :inverted="true" :circle="false" @click="toFile(1)"></uni-tag>
+					<uni-tag text="清" type="error" size="small" :inverted="true" :circle="false" @click="toFile(2)"></uni-tag>
+					<uni-tag text="资" type="error" size="small" :inverted="true" :circle="false" @click="toFile(3)"></uni-tag>
+				</view>
+			</view>
+			
+		</view>
+		
+		<!-- 没有数据 -->
+		<zywork-no-data v-else text="暂无招标信息"></zywork-no-data>
+		
 	</view>
 </template>
 
 <script>
+	import zyworkIcon from '@/components/zywork-icon/zywork-icon.vue'
+	import uniSegmentedControl from '@/components/uni-segmented-control/uni-segmented-control.vue'
+	import uniTag from '@/components/uni-tag/uni-tag.vue'
+	import zyworkNoData from '@/components/zywork-no-data/zywork-no-data.vue'
+	import {
+		IMAGE_BASE_URL,
+		DEFAULT_HEADICON,
+		isUserTokenExist,
+		toLoginPage,
+		getCalendarDate,
+		formatCalendarDate
+	} from '../../common/util.js'
+	
+	const PROJECT_STATUS_ALL = 0
+	const PROJECT_STATUS_SHOWING = 1
+	const PROJECT_STATUS_WAITTING = 2
+	const PROJECT_STATUS_ALREADY = 3
+	
+	const PROJECT_TYPE_BUILDING = 0
+	const PROJECT_TYPE_HYDRAULIC = 1
+	const PROJECT_TYPE_TRAFFIC = 2
+	const PROJECT_TYPE_PURCHASE = 3
+	const PROJECT_TYPE_IMPORTANT = 4
+	const PROJECT_TYPE_OTHER = 5
+	
+	const PROJECT_TYPE_ICONS = ['../../static/icon/building.png',
+								'../../static/icon/hydraulic.png',
+								'../../static/icon/traffic.png',
+								'../../static/icon/purchase.png',
+								'../../static/icon/important.png',
+								'../../static/icon/other.png']
 	export default {
+		components: {
+			zyworkIcon,
+			uniSegmentedControl,
+			uniTag,
+			zyworkNoData
+		},
 		data() {
 			return {
-
+				projectType: {
+					scrollLeft: 0,
+					tabIndex: 0,
+					tabbars: [
+						{id: 'building', name: '房建市政'},
+						{id: 'hydraulic', name: '水利工程'},
+						{id: 'traffic', name: '交通工程'},
+						{id: 'purchase', name: '政府采购'},
+						{id: 'important', name: '重点项目'},
+						{id: 'other', name: '其他项目'}
+					]
+				},
+				projectStatus: {
+					current: 0,
+					items: ['全部', '公告中', '待开标', '已开标']
+				},
+				imgIcon: PROJECT_TYPE_ICONS[0],
+				projectTypeName: '房建市政',
+				projects: [
+					{
+						title: '赣州中学工程招标',
+						city: '南昌市',
+						publishTime: '2019-04-24 18:00:00',
+						status: '公告中',
+						time: null,
+						checkWay: '资格后审',
+						money: 3000
+					},
+					{
+						title: '赣州中学工程招标',
+						city: '南昌市',
+						publishTime: '2019-04-24 18:00:00',
+						status: '待开标',
+						time: '2019-04-25 10:30',
+						checkWay: '资格后审',
+						money: 3000
+					}
+				],
+				pager: {
+					pageNo: 1,
+					pageSize: 10
+				},
 			}
 		},
-		onLoad() {},
-		methods: {}
+		onLoad() {
+		},
+		methods: {
+			getElSize(id) {
+				return new Promise((res, rej) => {
+					uni.createSelectorQuery().select("#" + id).fields({
+						size: true,
+						scrollOffset: true
+					}, (data) => {
+						res(data);
+					}).exec();
+				})
+			},
+			// 点击导航栏触发
+			async tapTab(e) {
+				let tabIndex = e.target.dataset.current
+				if (this.projectType.tabIndex === tabIndex) {
+					return false
+				} else {
+					let tabBar = await this.getElSize("tab-bar"),
+						tabBarScrollLeft = tabBar.scrollLeft
+					this.projectType.scrollLeft = tabBarScrollLeft
+					this.projectType.tabIndex = tabIndex
+					
+					this.imgIcon = PROJECT_TYPE_ICONS[tabIndex]
+				}
+			},
+			// 点击分段器触发
+			onClickItem(index) {
+				if (this.current !== index) {
+					this.current = index
+					if (index === PROJECT_STATUS_WAITTING) {
+						this.showChooseDate = true
+					} else {
+						this.showChooseDate = false
+					}
+				}
+			},
+			// 前往项目详情
+			toProjectDetail() {
+				uni.navigateTo({
+					url: '/pages-project-info/project-detail/project-detail'
+				})
+			},
+			// 查看文件
+			toFile(type) {
+				uni.showToast({
+					title: '查看文件:' + type,
+				})
+			}
+		}
 	}
 </script>
 
 <style lang="scss">
 	@import '../../common/zywork-main.scss';
+	@import '../../common/iconfont.css';
+	
+	.zy-project {
+		margin-top: 10upx;
+	}
+	
+	.zy-project-head {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+	
+	.zy-project-head .zy-icon {
+		width: 60upx;
+		height: 60upx;
+		margin-right: 15upx;
+	}
+	
+	.zy-project-head-right {
+		flex-grow: 1;
+		justify-content: flex-end;
+		text-align: right;
+	}
+	
+	.zy-project-item-row {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+	
+	.zy-project-item-row view {
+		width: 50%;
+	}
+	
+	.zy-project-item-file {
+		background-color: gainsboro;
+		text-align: right;
+		padding-right: 20upx;
+		uni-tag {
+			margin: 20upx 20upx;
+		}
+	}
+	
+	
 </style>
