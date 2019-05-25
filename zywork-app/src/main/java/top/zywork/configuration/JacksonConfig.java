9592ab5616e7d66d43a99200937ad8ea03df04d3
Carter <wjhsmarter@126.com>
2019-05-25 10:07:36 +0800

    1、appid换成正式的appid 2、补充提交后端代码

---
+++ zywork-app/src/main/java/top/zywork/configuration/JacksonConfig.java
@@ -0,0 +1,45 @@
+package top.zywork.configuration;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.io.IOException;
+
+
+/**
+ * Jackson配置类<br/>
+ *
+ * 创建于2019-05-24<br/>
+ *
+ * @author 王振宇
+ * @version 1.0
+ */
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    @Primary
+    @ConditionalOnMissingBean(ObjectMapper.class)
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
+            @Override
+            public void serialize(Object o, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+                if (o instanceof Number) {
+                    jsonGenerator.writeString("0");
+                } else {
+                    jsonGenerator.writeString("");
+                }
+            }
+        });
+        return objectMapper;
+    }
+}
+
--- zywork-app/src/main/java/top/zywork/constant/ProjectConstants.java
+++ zywork-app/src/main/java/top/zywork/constant/ProjectConstants.java
@@ -21,4 +21,21 @@ public final class ProjectConstants {
      * 中标公示状态-已开标
      */
     public static final String MARK_PUBLICIT_IN_MARK = "已开标";
+
+    /**
+     * 项目资源类型-澄清文件-0
+     */
+    public static final int PROJECT_SOURCE_TYPE_CHENGQING = 0;
+    /**
+     * 项目资源类型-招标文件-1
+     */
+    public static final int PROJECT_SOURCE_TYPE_ZHAOBIAO = 1;
+    /**
+     * 项目资源类型-清单文件-2
+     */
+    public static final int PROJECT_SOURCE_TYPE_QINGDAN = 2;
+    /**
+     * 项目资源类型-资质文件-3
+     */
+    public static final int PROJECT_SOURCE_TYPE_ZIZHI = 3;
 }
