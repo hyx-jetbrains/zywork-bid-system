9245dd4f145b17cc15927f3cfd28ead51af22c4f
Carter <wjhsmarter@126.com>
2019-06-05 17:36:32 +0800

    修改表长度

--- zywork-app/src/main/java/top/zywork/job/CompInfoHouseAgentJob.java
+++ zywork-app/src/main/java/top/zywork/job/CompInfoHouseAgentJob.java
@@ -7,10 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import top.zywork.annotation.ExposeClass;
+import top.zywork.constant.PythonConstants;
 import top.zywork.python.CompanyPythonService;
 
 /**
- * 访问python接口获取企业信息的作业<br/>
+ * 访问python接口获取房建代理公司信息的作业,
+ * 每半个小时执行一次<br/>
  *
  * 创建于2019-06-05<br/>
  *
@@ -18,16 +20,19 @@ import top.zywork.python.CompanyPythonService;
  * @version 1.0
  */
 @ExposeClass(type = "job")
-public class GetCompanyInfoJob implements Job {
+public class CompInfoHouseAgentJob implements Job {
 
-    private static final Logger logger = LoggerFactory.getLogger(GetCompanyInfoJob.class);
+    private static final Logger logger = LoggerFactory.getLogger(CompInfoHouseAgentJob.class);
 
     private CompanyPythonService companyPythonService;
+
+    public static int pageNo = 1;
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         logger.info("begin to execute GetCompanyInfoJob......");
-        companyPythonService.getCompanyInfo("3", "132", "1", "1");
+        companyPythonService.getCompanyInfo(PythonConstants.TYPE_AGENT, PythonConstants.COMP_TYPE_HOUSE_AGENT, String.valueOf(pageNo), "10");
+        pageNo++;
         logger.info("executed GetCompanyInfoJob......");
     }
 
